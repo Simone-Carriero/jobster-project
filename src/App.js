fca97e21cd8220139facb448b9fe001fda585b0b
Simone Carriero <simonecarriero95@hotmail.com>
2022-10-31 12:36:01 +0100

    Set basic routes and pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Landing from './pages/Landing';
+import { Landing, Dashboard, Register, Error } from './pages/index';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path='landing'
-          element={<Landing />}
-        />
+        <Route path='/'>
+          <Route
+            index
+            element={<Dashboard />}
+          />
+          <Route
+            path='landing'
+            element={<Landing />}
+          />
+          <Route
+            path='register'
+            element={<Register />}
+          />
+          <Route
+            path='*'
+            element={<Error />}
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
