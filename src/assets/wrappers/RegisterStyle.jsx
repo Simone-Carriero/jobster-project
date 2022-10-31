7c21b4d0f4e2fe304af7ff4c3b95c80c8218c2e6
Simone Carriero <simonecarriero95@hotmail.com>
2022-10-31 13:36:55 +0100

    Complete Register page

---
+++ src/assets/wrappers/RegisterStyle.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.main`
+  display: grid;
+  place-items: center;
+
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+
+  h3 {
+    text-align: center;
+  }
+
+  .btn {
+    margin-top: 1rem;
+  }
+`;
+export default Wrapper;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import Wrapper from '../assets/wrappers/RegisterStyle';
+
+import Logo from '../components/Logo';
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <Wrapper className='full-page'>
+      <section>
+        <form className='form'>
+          <Logo />
+          <h3>Login</h3>
+          <div className='form-row'>
+            <label
+              className='form-label'
+              htmlFor='email'>
+              Email
+            </label>
+            <input
+              type='email'
+              className='form-input'
+            />
+          </div>
+          <div className='form-row'>
+            <label
+              className='form-label'
+              htmlFor='password'>
+              Password
+            </label>
+            <input
+              type='password'
+              className='form-input'
+            />
+          </div>
+
+          <button className='btn btn-block'>Submit</button>
+        </form>
+      </section>
+    </Wrapper>
+  );
 };
 
 export default Register;
