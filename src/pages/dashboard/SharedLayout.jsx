fa4f96e44a508e8c8a26f4dd380a9f40c363a5db
Simone Carriero <simonecarriero95@hotmail.com>
2022-11-03 12:29:32 +0100

    Create basic components for SharedLayout

--- src/pages/dashboard/SharedLayout.jsx
+++ src/pages/dashboard/SharedLayout.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Wrapper from '../../assets/wrappers/SharedLayoutStyle';
+import Navbar from '../../components/Navbar';
+import SmallSidebar from '../../components/SmallSidebar';
+import BigSidebar from '../../components/BigSidebar';
 
 const SharedLayout = () => {
-  return <div>SharedLayout</div>;
+  return (
+    <Wrapper>
+      <SmallSidebar />
+      <BigSidebar />
+      <div>
+        <Navbar />
+        <main className='dashboard-page'>
+          <Outlet />
+        </main>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SharedLayout;
