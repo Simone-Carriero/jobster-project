f1da6a06a446d6b1951daaa86b7e27f775743b9f
Simone Carriero <simonecarriero95@hotmail.com>
2022-11-04 20:13:40 +0100

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
 import Logo from './Logo';
 import Wrapper from '../assets/wrappers/NavbarStyle';
 import { useSelector, useDispatch } from 'react-redux';
-import { toggleSidebar } from '../features/user/userSlice';
+import { toggleSidebar, logoutUser } from '../features/user/userSlice';
 
 const Navbar = () => {
   const { user } = useSelector((state) => state.user);
@@ -38,7 +38,10 @@ const Navbar = () => {
           <div className={`dropdown ${showLogout && 'show-dropdown'}`}>
             <button
               type='button'
-              className='dropdown-btn'>
+              className='dropdown-btn'
+              onClick={() => {
+                dispatch(logoutUser());
+              }}>
               Logout
             </button>
           </div>
