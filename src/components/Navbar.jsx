c4cc8b1927f944c3601d98d14117e975e077865e
Simone Carriero <simonecarriero95@hotmail.com>
2022-11-04 13:24:55 +0100

    Complete Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
+import Logo from './Logo';
+import Wrapper from '../assets/wrappers/NavbarStyle';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleSidebar } from '../features/user/userSlice';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { user } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const [showLogout, setShowLogout] = useState(false);
+
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
+  return (
+    <Wrapper>
+      <div className='nav-center'>
+        <button
+          type='button'
+          className='toggle-btn'
+          onClick={toggle}>
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className='logo-text'>dashboard</h3>
+        </div>
+        <div className='btn-container'>
+          <button
+            type='button'
+            className='btn'
+            onClick={() => setShowLogout(!showLogout)}>
+            <FaUserCircle />
+            {user.name}
+            <FaCaretDown />
+          </button>
+          <div className={`dropdown ${showLogout && 'show-dropdown'}`}>
+            <button
+              type='button'
+              className='dropdown-btn'>
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Navbar;
