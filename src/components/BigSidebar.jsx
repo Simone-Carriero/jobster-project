15c219d9d8ce0d7f34b6092a1ba1851c316c9c32
Simone Carriero <simonecarriero95@hotmail.com>
2022-11-05 14:44:43 +0100

    Complete BigSidebar

--- src/components/BigSidebar.jsx
+++ src/components/BigSidebar.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Wrapper from '../assets/wrappers/BigSidebarStyle';
+import Logo from './Logo';
+import NavLinks from './NavLinks';
 
 const BigSidebar = () => {
-  return <div>BigSidebar</div>;
+  const { isSidebarOpen } = useSelector((store) => store.user);
+
+  return (
+    <Wrapper>
+      <div className={`sidebar-container ${isSidebarOpen && 'show-sidebar'}`}>
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default BigSidebar;
